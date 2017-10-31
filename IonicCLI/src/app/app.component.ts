9bac60785a5a02ed2a7e99ce1f0874527f6b19e6
slooock <kayque.avelar@gmail.com>
2017-10-31 14:51:25 -0200

    oi

---
+++ IonicCLI/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+  pages: [{ title: String, component: any }]
+  rootPage: any = HomePage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+    this.pages = [
+      { title: "Home Page", component: HomePage }
+
+    ];
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
+  openPage(page: { title: string, component: any }): void {
+    this.nav.setRoot(page.component);
+  }
+}
+
